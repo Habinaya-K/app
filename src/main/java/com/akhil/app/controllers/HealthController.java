43d9a466282abafc1997dab02c5e25af035d4864
Habinaya Krishnamoorthy <habik902@gmail.com>
2024-04-05 13:07:02 +0530

    Changed the endpoint as "heathchecker"

--- src/main/java/com/akhil/app/controllers/HealthController.java
+++ src/main/java/com/akhil/app/controllers/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-    @GetMapping("/health")
+    @GetMapping("/healthcheck")
     public String health(){
         boolean isUp = true;
         if (isUp){
